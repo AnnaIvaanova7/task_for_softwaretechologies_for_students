37575597e09320fbf0c9d9ab49fa2c3378b252b8
aaivanova7 <ivanna13122005@gmail.com>
2024-10-11 11:48:58 +0300

    init commit

--- src/main/java/org/softwaretechnologies/ArrayFunctions.java
+++ src/main/java/org/softwaretechnologies/ArrayFunctions.java
@@ -10,6 +10,14 @@ public final class ArrayFunctions {
      * @param array массив, который будет перевернут.
      */
     public static void reverse(int[] array) {
+        int n = array.length;
+        for (int i=0; i <= n/2; i++)
+        {
+            int b = array[i];
+            array[i]=array[n-1-i];
+            array[n-1-i]=b;
+
+        }
         // TODO: реализуйте вышеуказанную функцию
     }
 
@@ -23,7 +31,24 @@ public final class ArrayFunctions {
        Матрица не квадратная
      * @param matrix матрица, в которой столбцы будут заменены на строки.
      */
-    public static void rotateMatrix(int[][] matrix) {
+    public static void rotateMatrix(int[][] matrix)
+    {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        if (n != m)
+        {
+            System.out.println("Error");
+        }
+        for (int i = 0; i<=n; i++)
+        {
+            for (int j = 0; j<=n; j++)
+            {
+                int b = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = b;
+            }
+        }
+
         // TODO: реализуйте вышеуказанную функцию
     }
 }
